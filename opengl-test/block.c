dc4aef3f7d366efa403d5e3be9d55ff826cf21f2
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-04 14:15:04 +0900

    impl remove block

--- opengl-test/block.c
+++ opengl-test/block.c
@@ -11,3 +11,15 @@ void addBlock(int x, int y, int z) {
     blocks[blockLength] = makeBlock(x, y, z);
     blockLength = blockLength + 1;
 }
+void removeBlock(int x, int y, int z) {
+    int resultIndex = -1;
+    for (int i = 0; i < blockLength; i++) {
+        if (blocks[i].x == x && blocks[i].y == y && blocks[i].z == z) {
+            resultIndex = i;
+            break;
+        }
+    }
+    if (resultIndex == -1) return;
+    
+    for (int i = resultIndex; i < blockLength - 1; i++) blocks[i] = blocks[i + 1];
+}
